144e47e365fe7741047d2d09b832cc87653c92d8
Chefjdeep <jaykokn@gmail.com>
2024-12-06 18:00:52 +0530

    readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 Kalakari College Cultural Group. All rights reserved.</p>
+      <p>&copy; 2024 Kalakari College Cultural Group</p>
+      <p>Made by Jaydeep</p>
     </footer>
   );
 };
